da8220902c7c8a8895ec2462a7f66bd95fdc23d4
Dylan Coleman <dylancoleman7199@gmail.com>
2020-04-11 01:27:36 -0400

    Messing around with item system

--- src/Main.js
+++ src/Main.js
@@ -10,19 +10,20 @@ setup.skills = function PlayerNPCSkills(unarmed, melee, ranged, fire_magic, ice_
     this.salesmenship = salesmanship;
 };
 
-setup.stats = function PlayerNPCs(name, hp, str, int, dex, agl, per, chs, race) {
+setup.stats = function PlayerNPCs(name, base_hp, current_hp, str, int, end, agl, per, chs, race) {
     this.name = name;
-    this.hp = hp;
+    this.base_hp = base_hp;
+    this.current_hp = current_hp;
     this.str = str;
     this.int = int;
-    this.end = dex;
+    this.end = end;
     this.agl = agl;
     this.per = per;
     this.chs = chs;
     this.race = race;
 };
 
-setup.moves =function Moves (atkName, damage, accuracy, flavor) {
+setup.moves = function Moves (atkName, damage, accuracy, flavor) {
     /*Might want to add a parameter signifying preference the AI has for an attack. DIO, for example, should not use
     * ZA WAURDO every turn. I could work some probability in to keep this from happening.*/
     this.atkName = atkName;
@@ -32,22 +33,40 @@ setup.moves =function Moves (atkName, damage, accuracy, flavor) {
 
 };
 
+setup.items = function Items (name, effect, flavor_text) {
+    this.name = name;
+    this.effect = effect;
+    this.flavor_text = flavor_text;
+};
+
 setup.initstats = function InitStats() {
 
     window.hyenaMugger = new setup.stats("Hyena Mugger", 15,2,2,3,4,1,1, "Hyena");
     window.tigerMugger = new setup.stats("Armed Tiger Mugger", 15,2,23,4,1,1, "Tiger");
-    window.playerStats = new setup.stats("", 50, 5,5,5,5,5,5, "");
+    window.playerStats = new setup.stats("", 50, "", 5,5,5,5,5,5, "");
     window.playerSkills = new setup.skills(10,10,10,10,10,10,10,10);
 
     window.punchAttack = new setup.moves("Punch", 3, .80, " sends a fast punch right towards your chest.");
     window.knifeSlash = new setup.moves("Knife Slash", 8, .65, "pulls a knife on you, slashing a non-vital spot.");
     window.playerPunch = new setup.moves("Light Punch", 5, .90, "You punch the " + enemyFighting.name);
-};
 
+    window.testItemHeals = new setup.items("Testing healing potion", playerStats.current_hp += 10, "This works nice.");
+    playerItems.push(testItemHeals.name);
+};
+setup.itemTest = function Test() {
+    window.choosenPlayerItem = "";
+    state.active.variables["itemChoosen"] = choosenPlayerItem;
+    if (choosenPlayerItem in playerItems.name) {
+        console.log("Works in theory or something");
+    }
+};
 setup.whogoes = function StartingObjs() {
     /*Currently initializes lots of variables and creates the basic NPC and player objects. Lots of the stuff here
     * will be moved to different functions later. This is the quick and dirty way to get stuff working while I
     * muck about. */
+
+    window.playerItems = [];
+
     window.enemyFighting = "";
 
     window.playerAttack = "";
@@ -68,7 +87,7 @@ setup.PCAttacking = function EnemyDamage() {
     console.log(playerAttackChance);
 
     if (playerAttackChance <= playerAttack.accuracy) {
-        enemyFighting.hp -= playerAttackCalc;
+        enemyFighting.current_hp -= playerAttackCalc;
         /*Player attack is done.*/
         playerAttack = "";
         playerAttackHitMiss = true;
@@ -96,8 +115,8 @@ setup.enemyAttack = function EnemyAttacking() {
     window.choosenAttack = getArrayRandomElement(enemyAttacks);
     window.enemyAttackCalc = choosenAttack.damage + 0.20 * enemyFighting.str | 0;
     const enemyAttackChance = Math.random();
-    if (enemyAttackChance <= choosenAttack.accuracy) {
-        playerStats.hp -= enemyAttackCalc;
+    if (enemyAttackChance <= choosenAttack.accuracy - (playerStats.agl / 150)) {
+        playerStats.current_hp -= enemyAttackCalc;
     } else {
         enemyAttackHitMiss = true;
         console.log("You expected a hit, but it was me, Dio!");
@@ -107,6 +126,7 @@ setup.enemyAttack = function EnemyAttacking() {
 setup.raceSettings = function racialTraits() {
     state.active.variables["name"] = playerStats.name;
     state.active.variables["race"] = playerStats.race;
+    state.active.variables["itemChoosen"] = playerStats.name;
     if (playerStats.race === "Wolf") {
         playerStats.str += 1;
         playerStats.per += 1;
@@ -151,35 +171,6 @@ $(document).on('click', 'a', function (ev) {
 
 });*/
 
-function RaceSetting() {
-    const race = state.active.variables.race;
-
-    if (race === "Wolf") {
-        PlayerStats.str += 1;
-        PlayerStats.per += 1;
-        PlayerSkills.unarmed += 5
-    };
-
-    if (race === "Lion") {
-        PlayerStats.agl += 1;
-        PlayerStats.int += 1;
-        PlayerSkills.mind_magic += 5
-    };
-
-    if (race === "Dragon") {
-        PlayerStats.str += 1;
-        PlayerStats.agl += 1;
-        PlayerSkills.ice_magic += 5;
-        PlayerSkills.fire_magic += 5
-    };
-
-    if (race === "Fox") {
-        PlayerStats.dex += 1;
-        PlayerStats.chs += 1;
-        PlayerSkills.ranged += 5
-    };
-
-}
 
 if (choosenAttack.name === "MUDA") {
     $(output).wiki("DIO attacks with " + choosenAttack.name + ", unleashing a volley of incredibly fast fists at you." +
